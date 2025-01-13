c879511e7a0326812e55ae406eb32199d52f9cc9
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-12 22:40:55 -0800

    enable osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
 
 if [[ "${target_platform}" == osx-* ]]; then
   export CXXFLAGS="$CXXFLAGS -std=c++14"
 fi
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
 
 ./configure --prefix=${PREFIX}
 make
